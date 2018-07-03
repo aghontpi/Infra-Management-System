46daa00045917226985543755084d706ec96f9c3
Gopinath (aka) BluePie <gopinath2nr@gmail.com>
2018-07-03 09:47:28 +0530

    backup of current development

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,5 @@
 <div class="dashboard-container">
+	<div class="overall-container">
 	<div class="dashboard">
 		<span>
 			This is the admin dashboard, here you can view and approve new reqistrations, view current items that are loned for employees and search for an item based on category.<br>
@@ -7,8 +8,18 @@
 			New user registrations to approve :
 		</span>
 	</div>
-	<div>
-	
+	<div class="search-container">
+		<div class="search" id="category_selector">
+			<div><input type="text" name="search_field" id="search-field" placeholder="search-term"></div>
+			 <div><select name="What" placeholder="What?" id="what">
+    			<option value="user">Users</option>
+    			<option value="device">Devices</option>
+  			</select>
+  			</div>	
+		</div>
+		<div class="search-go">
+			<span id="search_button"  name="search_btn" >Go</span>
+		</div>
 	</div>
-	
+	</div>
 </div>
