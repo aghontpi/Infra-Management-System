ca77386bf018a049fcd9b57c947c62d9a8b1c1b0
Gopinath (aka) BluePie <gopinath2nr@gmail.com>
2018-12-05 23:59:41 +0530

    changed cdn to local.

--- users.php
+++ users.php
@@ -25,10 +25,11 @@ $count = $db->getNumberPendingAccounts();
 	<link rel="stylesheet" href="css/main.css" />
 	<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
-	<link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"/>
- 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.jqueryui.min.css"/>
-	<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-	<script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.jqueryui.min.js"></script>
+	<!---------datatable's used here are 1.9 --->
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.css"/>
+ 	<link rel="stylesheet" type="text/css" href="css/dataTables.jqueryui.min.css"/>
+	<script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+	<script type="text/javascript" src="js/dataTables.jqueryui.min.js"></script>
 </head>
 <body style="background-color:#083045;">
 	<?php include("header.php"); ?>
